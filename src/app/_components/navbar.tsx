0ed8d18e4969eb1eeceff7b2b81e6c5314c69bf9
hnakae <nakae27@gmail.com>
2024-05-30 22:50:12 -0700

    nav ul styles

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -17,7 +17,7 @@ export function Navbar() {
           Eugene Go Club
         </Link>
       </h1>
-      <ul className="flex space-x-4 px-8 items-center text-xl tracking-tighter leading-tight">
+      <ul className="flex space-x-4 px-8 items-center text-l tracking-tighter leading-tight font-semibold">
         {/* <li className="cursor-pointer hover:underline rounded-md hover:bg-slate-200 px-4 py-2">
           <Link href="/contact">Contact</Link>
         </li> */}
