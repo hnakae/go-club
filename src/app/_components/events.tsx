256d0e1e8df8ca07f549f9fcd653018ebb336ea2
hnakae <nakae27@gmail.com>
2024-06-13 19:12:38 -0700

    events page

--- src/app/_components/events.tsx
+++ src/app/_components/events.tsx
@@ -3,33 +3,34 @@ import Image from "next/image";
 export function Events() {
   return (
     <>
-      <div className=" px-6 bg-[#ffffff]  h-fit flex-col flex items-start  justify-center max-w-[1000px]  border-black border-b">
-        <div className="font-playfair font-semibold  text-[32px]    tracking-tighter leading-tight  ">
+      <div className=" px-6 bg-[#ffffff]  h-fit flex-col flex items-start  justify-center max-w-[1000px]  ">
+        <div className="font-playfair font-semibold  text-[32px] mb-12   tracking-tighter leading-tight border-b w-full border-black ">
           Events
         </div>
-      </div>
-      <a
-        className=" text-[24px] hover:underline text-blue-700 font-bold border"
-        href="https://gocongress.org/"
-        target="_blank"
-      >
-        <div className=" bg-white flex items-center justify-center  ">
-          <div className="">
-            <Image
-              src="/assets/images/40thUSGoCongress.png"
-              alt="image"
-              width={200}
-              height={200}
-            />
-          </div>
-          <div className="flex flex-col justify-center items-center">
-            <div className="font-playfair font-bold text-3xl mb-3">
-              US Go Congress 2024
+
+        <a
+          className=" text-[24px] hover:underline text-blue-700 font-bold shadow-md rounded-md px-4 py-2"
+          href="https://gocongress.org/"
+          target="_blank"
+        >
+          <div className=" bg-white flex items-center justify-center  ">
+            <div className="">
+              <Image
+                src="/assets/images/40thUSGoCongress.png"
+                alt="image"
+                width={200}
+                height={200}
+              />
             </div>
-            <div className="font-montserrat">July 13-21</div>
+            <div className="flex flex-col justify-center items-center">
+              <div className="font-playfair font-bold text-3xl mb-3">
+                US Go Congress 2024
+              </div>
+              <div className="font-montserrat">July 13-21</div>
+            </div>
           </div>
-        </div>
-      </a>
+        </a>
+      </div>
     </>
   );
 }
