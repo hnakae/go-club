7117e27ed53d959f016ee196249f67aa7ae2c581
hnakae <nakae27@gmail.com>
2024-06-17 22:26:12 -0700

    style footer like hero section

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -7,79 +7,14 @@ import { usePathname } from "next/navigation";
 export function Footer() {
   const pathname = usePathname();
   return (
-    // #A69EF5", to: "#DCC9FE" bg-gradient-to-tr from-[#A69EF5] to-[#DCC9FE]
-    // <footer className="select-none max-w-screen mx-0 bg-slate-600 flex justify-center px-6">
-    //   {/* <Container> */}
-    //   {/* <div className="w-full max-w-[1000px] flex justify-between items-center "> */}
-    //   <div className="w-full max-w-[1000px] py-28 flex flex-col items-start text-white ">
-    //     <h3 className="font-playfair text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2 ">
-    //       Eugene Go Club
-    //     </h3>
-
-    //     <h1 className="font-montserrat">
-    //       Eugene Go Club is a free weekly club for go enthusiasts.
-    //     </h1>
-
-    //     <div className="socials flex pt-4  space-x-4">
-    //       <ul className="font-montserrat text-white  flex space-x-2  items-center tracking-tighter leading-tight xs:hidden  ">
-    //         <li className="cursor-pointer rounded-md  px-4 py-2">
-    //           <Link href="/faqs">FAQs</Link>
-    //         </li>
-    //         <li className="cursor-pointer rounded-md  px-4 py-2">
-    //           <Link href="/events">Events</Link>
-    //         </li>
-    //       </ul>
-    //       <Link
-    //         href="/https://www.facebook.com/groups/EugeneGoClub"
-    //         target="_blank"
-    //       >
-    //         <Image
-    //           src="/icons/facebook.png"
-    //           alt="facebook icon"
-    //           width={25}
-    //           height={25}
-    //         />
-    //       </Link>
-    //       <Link
-    //         href="https://discord.com/channels/1164649557687275703/1174766988787208345"
-    //         target="_blank"
-    //       >
-    //         <Image
-    //           src="/icons/discord.png"
-    //           alt="facebook icon"
-    //           width={25}
-    //           height={25}
-    //         />
-    //       </Link>{" "}
-    //       <Link
-    //         href="/https://www.meetup.com/eugene-go-players/"
-    //         target="_blank"
-    //       >
-    //         <Image
-    //           src="/icons/meetup.png"
-    //           alt="facebook icon"
-    //           width={25}
-    //           height={25}
-    //         />
-    //       </Link>{" "}
-    //       <Link href="/https://online-go.com/group/6435" target="_blank">
-    //         <Image
-    //           className="rounded-full"
-    //           src="/icons/ogs-icon.png"
-    //           alt="facebook icon"
-    //           width={25}
-    //           height={25}
-    //         />
-    //       </Link>
-    //     </div>
-    //   </div>
-    //   {/* </Container> */}
-    // </footer>
-    <footer className=" w-full  px-24 font-normal text-sm text-dark border-t bg-brandWhite sm:text-base  ">
+    <footer className="relative w-full  px-24 font-normal text-sm text-dark border-t bg-brandWhite sm:text-base  ">
+      <div className="absolute bottom-24  text-[140px] font-sans font-bold text-gray-100 z-0">
+        Eugene Go Club
+      </div>
       <div className="w-full ">
         <div className="flex justify-around py-12">
           <div className="flex flex-col gap-4">
-            <div className=" px-5   flex-col flex items-center justify-center rounded-md  text-center   z-20">
+            <div className=" px-5   flex-col flex items-center justify-center rounded-md  text-center z-10  ">
               {/* <div className="text-lg font-montserrat  ">Meetup Info</div> */}
               <div className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl font-playfair">
                 {" "}
@@ -121,7 +56,7 @@ export function Footer() {
               <GithubIcon className="lg:w-[30px]" /> */}
             </div>
           </div>
-          <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-4 z-10">
             <div className="font-bold text-2xl">Browse</div>
 
             <Link
@@ -160,7 +95,7 @@ export function Footer() {
               Events
             </Link>
           </div>
-          <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-4 z-10">
             <div className="font-bold text-2xl">Activities</div>
             <div>Play Games</div>
             <div>Study Pro Games</div>
@@ -168,7 +103,7 @@ export function Footer() {
             <div>Discuss Strategy</div>
             <div>Tournaments</div>
           </div>
-          <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-4 z-10">
             <div className="font-bold text-2xl">Contact</div>
             <div className="flex ">
               <Image
@@ -205,20 +140,15 @@ export function Footer() {
       </div>
       <div className=" flex items-center justify-between py-4 px-12  ">
         {/* <Logo /> */}
-        <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
-        <div className="flex items-center lg:py-2">
+        <span className="z-10">
+          {new Date().getFullYear()} &copy; All Rights Reserved.
+        </span>
+        <div className="flex items-center lg:py-2 z-10">
           Built With
           <span className="text-brand text-2xl px-1 dark:text-primaryDark">
             &#9825;
           </span>
-          by&nbsp;
-          {/* <Link */}
-          {/* href="/" */}
-          {/* target={"_blank"} */}
-          {/* className="underline underline-offset-2 " */}
-          {/* > */}
-          Hiro Nakae
-          {/* </Link> */}
+          by&nbsp; Hiro Nakae
         </div>
       </div>
     </footer>
