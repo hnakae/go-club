37932c173e979bb685d449937e9b9866b7ffdb6a
hnakae <nakae27@gmail.com>
2024-06-20 21:48:22 -0700

    post-header

--- src/app/_components/post-header.tsx
+++ src/app/_components/post-header.tsx
@@ -15,7 +15,7 @@ export function PostHeader({ title, coverImage, date, author }: Props) {
   return (
     <div className="border mx-6 px-14 py-10 max-w-[1128px] border-primaryColor rounded-md mb-12 shadow-md p-8  backdrop-blur-md">
       <div className="mt-6 ">
-        <PostTitle>{title} Review</PostTitle>
+        <PostTitle>{title}</PostTitle>
       </div>
       <div className="flex mb-6 justify-start gap-4">
         {/* <Avatar name={author.name} picture={author.picture} /> */}
