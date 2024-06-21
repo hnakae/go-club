b06bf711f5616a1901cb83b8a8b11102c83c9fdb
hnakae <nakae27@gmail.com>
2024-06-21 01:17:10 -0700

    meetup info z-index

--- src/app/_components/meetup-info.tsx
+++ src/app/_components/meetup-info.tsx
@@ -11,7 +11,7 @@ const MeetUpInfo = () => {
         {/* Preload images */}
         <link rel="preload" href="/assets/images/stones.jpg" as="image" />
       </Head>
-      <div className="relative my-12  mx-6 flex items-center justify-center  sm:flex-col xs:flex-col sm:mt-0 xs:mt-0 xs:bg-[#DFF8B4] sm:mb-6  ">
+      <div className="relative my-12 z-20 mx-6 flex items-center justify-center  sm:flex-col xs:flex-col sm:mt-0 xs:mt-0 xs:bg-[#DFF8B4] sm:mb-6  ">
         {/* <div className="fixed z-0 top-0 h-full w-full text-[200px] font-sans font-bold text-gray-200 bg-[url(/assets/images/slatebg.png)]" /> */}
 
         <div className="max-w-[1128px] rounded-md flex  justify-center items-center  w-full  text-neutralColor z-10 border border-primaryColor backdrop-blur-md">
