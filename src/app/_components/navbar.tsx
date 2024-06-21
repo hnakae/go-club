506549903d29cb9fbae2cb86b2a83a8a1fd4eb8e
hnakae <nakae27@gmail.com>
2024-06-21 01:14:26 -0700

    nav z index when hidden

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -45,7 +45,7 @@ export function Navbar() {
       {/**/}
       <section
         className={`z-50  text-neutralColor transition-all duration-200 linear  backdrop-blur-xl bg-darkNav	 sticky top-0 flex justify-center items-center  py-2 w-full mx-0 px-6  select-none ${
-          scrollDirection === "down" ? "-translate-y-full" : "translate-y-0"
+          scrollDirection === "down" ? "-translate-y-full z-0" : "translate-y-0"
         }`}
       >
         <div className="w-full  flex justify-between items-center">
