212c6a22eca2848b1d73d37c25b1cc2f99677dd8
hnakae <nakae27@gmail.com>
2024-06-25 12:02:03 -0700

    post body

--- src/app/_components/post-body.tsx
+++ src/app/_components/post-body.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export function PostBody({ content }: Props) {
   return (
-    <div className=" w-full mb-4 xs:px-4 sm:px-6 md:px-20 lg:px-44 xl:px-44 2xl:px-48 py-4 border border-primaryColor rounded-md shadow-md   backdrop-blur-md">
+    <div className=" w-full mb-4 xs:px-4 sm:px-6 md:px-20 lg:px-44 xl:px-44 2xl:px-48  border border-primaryColor rounded-md shadow-md   backdrop-blur-md">
       <div
         className={markdownStyles["markdown"]}
         dangerouslySetInnerHTML={{ __html: content }}
