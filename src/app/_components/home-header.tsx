76dafe11e8907224ee35aa4833a83653f8354b6d
hnakae <nakae27@gmail.com>
2024-06-25 23:59:58 -0700

    prisma orm, docker, route handler

--- src/app/_components/home-header.tsx
+++ src/app/_components/home-header.tsx
@@ -1,16 +1,30 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Quote from "../contact/quote";
 import Button from "./ui/cta-button";
 import Head from "next/head";
 import MeetupInfo from "./meetup-info";
 
 const HomeHeader = () => {
+  const [resultFromRouteHandler, setResultFromRouteHandler] = useState();
+  useEffect(() => {
+    const getResult = async () => {
+      const response = await fetch("../api/routeHandler");
+      const data = await response.json();
+
+      setResultFromRouteHandler(data.message);
+    };
+    getResult();
+  }, []);
+
   return (
     <>
       <div className="flex justify-center items-center  z-10 w-full   ">
         <div className="    py-10 my-4 max-w-[1128px]  rounded-md flex  justify-center items-center  w-full xs:border-none xs:my-0  text-neutralColor z-20 border border-primaryColor backdrop-blur-md">
           <div className=" w-full px-10  flex sm:flex-col-reverse xs:flex-col-reverse items-center justify-around rounded-md  text-neutralColor    z-20  border-primaryColor ">
+            {resultFromRouteHandler}
+
             <MeetupInfo />
             <Image
               width={400}
