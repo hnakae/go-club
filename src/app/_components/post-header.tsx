121e2eddfb89bcf14d3c5f59cd650c8535e323ce
hnakae <nakae27@gmail.com>
2024-06-25 20:53:17 -0700

    post header

--- src/app/_components/post-header.tsx
+++ src/app/_components/post-header.tsx
@@ -20,7 +20,7 @@ export function PostHeader({
 }: Props) {
   return (
     <div className="flex justify-center items-center  z-10 w-full   ">
-      <div className="    py-10 my-4 max-w-[1128px]  rounded-md flex  justify-center items-center  w-full xs:border-none xs:my-0  text-neutralColor z-20 border border-primaryColor backdrop-blur-md">
+      <div className="    py-10 my-4 max-w-[1128px] xs:px-4 sm:px-6 md:px-20 lg:px-44 xl:px-44 2xl:px-48 rounded-md flex  justify-center items-center  w-full xs:border-none xs:my-0  text-neutralColor z-20 border border-primaryColor backdrop-blur-md">
         <div className=" w-full px-10  flex sm:flex-col-reverse xs:flex-col-reverse  items-center justify-around rounded-md  text-neutralColor    z-20  border-primaryColor ">
           <div className="flex flex-col items-start mt-4">
             <PostTitle>{title}</PostTitle>
