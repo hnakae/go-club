2d5922022da7d3431306d97d59a02adb39a29efa
hnakae <nakae27@gmail.com>
2024-07-03 09:57:22 -0700

    strength of stones

--- src/app/_components/mdx/ImageText.tsx
+++ src/app/_components/mdx/ImageText.tsx
@@ -10,10 +10,10 @@ interface ImageTextProps {
 const ImageText = ({ src, text1, text2 }: ImageTextProps) => {
   return (
     <div className="flex w-full mt-3 pb-8">
-      <div className="w-1/3 max-w-1/3 rounded-sm overflow-hidden">
+      <div className="w-1/3 rounded-sm overflow-hidden">
         <Image src={src} alt="image" width={600} height={600} />
       </div>
-      <div className="flex flex-col justify-center">
+      <div className="w-2/3 flex flex-col justify-center">
         <div className="max-w-[500px] p-3 flex items-center">{text1}</div>
         <div className="max-w-[500px] p-3 flex items-center">{text2}</div>
       </div>
