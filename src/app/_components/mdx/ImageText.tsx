6072cde06bb55a20b669d766aa8db8659f3f5fc7
hnakae <nakae27@gmail.com>
2024-07-03 06:35:39 -0700

    direction formatting

--- src/app/_components/mdx/ImageText.tsx
+++ src/app/_components/mdx/ImageText.tsx
@@ -3,17 +3,20 @@ import Image from "next/image";
 
 interface ImageTextProps {
   src: string;
-  text: string;
+  text1: string;
+  text2: string;
 }
 
-const ImageText = ({ src, text }: ImageTextProps) => {
+const ImageText = ({ src, text1, text2 }: ImageTextProps) => {
   return (
-    <div className="flex w-full mt-3">
-      <div className="w-full rounded-sm overflow-hidden">
+    <div className="flex w-full mt-3 pb-8">
+      <div className="w-1/3 max-w-1/3 rounded-sm overflow-hidden">
         <Image src={src} alt="image" width={600} height={600} />
       </div>
-
-      <div className="max-w-[500px] p-3 ">{text}</div>
+      <div className="flex flex-col justify-center">
+        <div className="max-w-[500px] p-3 flex items-center">{text1}</div>
+        <div className="max-w-[500px] p-3 flex items-center">{text2}</div>
+      </div>
     </div>
   );
 };
