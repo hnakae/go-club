3f1e99023fe6a684c15baeb640ac71455a28999d
hnakae <nakae27@gmail.com>
2024-07-04 20:00:32 -0700

    responsive image-text component

--- src/app/_components/mdx/ImageText.tsx
+++ src/app/_components/mdx/ImageText.tsx
@@ -9,11 +9,11 @@ interface ImageTextProps {
 
 const ImageText = ({ src, text1, text2 }: ImageTextProps) => {
   return (
-    <div className="flex w-full mt-3 pb-8">
-      <div className="w-1/3 rounded-sm overflow-hidden">
+    <div className="flex w-full mt-3 pb-8 sm:flex-col xs:flex-col justify-center items-center">
+      <div className="w-1/3 sm:w-full xs:w-full rounded-sm overflow-hidden">
         <Image src={src} alt="image" width={600} height={600} />
       </div>
-      <div className="w-2/3 flex flex-col justify-center">
+      <div className="w-2/3 sm:w-full xs:w-full flex flex-col justify-center">
         <div className="max-w-[500px] p-3 flex items-center">{text1}</div>
         <div className="max-w-[500px] p-3 flex items-center">{text2}</div>
       </div>
