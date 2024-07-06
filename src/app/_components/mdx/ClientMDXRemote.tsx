e999ec0c7516f91d7955e497db591498770afcba
hnakae <nakae27@gmail.com>
2024-07-06 11:21:25 -0700

    typo

--- src/app/_components/mdx/ClientMDXRemote.tsx
+++ src/app/_components/mdx/ClientMDXRemote.tsx
@@ -6,12 +6,13 @@ import dynamic from "next/dynamic";
 import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
 import Button from "../ui/cta-button";
 import ImageText from "./ImageText";
+import IframeComponent from "./iframeComponent";
 
 const EidoGoViewer = dynamic(() => import("../eidogo/EidogoViewer"), {
   ssr: false,
 });
 
-const components = { Button, EidoGoViewer, ImageText };
+const components = { Button, EidoGoViewer, ImageText, IframeComponent };
 
 const ClientMDXRemote = (props: MDXRemoteSerializeResult) => (
   <MDXRemote {...props} components={components} />
