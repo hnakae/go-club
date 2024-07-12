075252bf016a3f1af0b53c4a86dc63b317003fc5
hnakae <nakae27@gmail.com>
2024-07-11 18:01:54 -0700

    about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -18,11 +18,29 @@ export default function About() {
       <Container>
         <Header />
 
+        {/* about */}
+        <section className="w-full flex justify-center items-start z-10 text-neutralColor mb-4">
+          <div className=" max-w-[1128px]  px-14 py-10  flex-col flex items-start justify-start rounded-md  border border-primaryColor  shadow-md   backdrop-blur-md w-full  ">
+            <p className="mb-3 text-xl ">About us</p>
+            <br />
+            {/* <p className="text-lg mb-3">Are beginners welcome?</p> */}
+            <p className="mb-6">
+              Open to players of all skill levels and complete beginners who
+              want to learn! (Feel free to bring a board if you have one!)
+            </p>
+            <p className="mb-6">
+              *We will endeavor to post updates about meeting times/places in
+              our Discord Server, and will also try to update the facebook page
+              and meetup page, but the Discord will always be up to date and is
+              the best place to get in contact with us
+            </p>
+          </div>
+        </section>
         {/* faq */}
         <section className="w-full flex justify-center items-start z-10 text-neutralColor mb-4">
           <div className=" max-w-[1128px]  px-14 py-10  flex-col flex items-start justify-start rounded-md  border border-primaryColor  shadow-md   backdrop-blur-md w-full  ">
             <p className="mb-3 text-xl ">FAQs</p>
-
+            <br />
             <p className="text-lg mb-3">Are beginners welcome?</p>
             <p className="mb-6">
               Beginners are absolutely welcome! Beginners can find similar level
@@ -32,8 +50,8 @@ export default function About() {
             <p className="text-lg mb-3">How can we get in contact?</p>
             <p className="mb-6">
               Discord is always up to date and will be the best place to reach
-              either the host or any of the regulars who can help with any
-              questions you may have. The Discord link is in the Contact Page.
+              someone who can help with any questions. The Discord link is in
+              the Contact Page.
             </p>
 
             {/* <div>Event Calendar coming soon</div> */}
@@ -44,21 +62,12 @@ export default function About() {
           </div>
         </section>
         {/* images */}
-        <section className="flex items-center w-full max-w-[1128px] xs:hidden sm:hidden  md:hidden mb-4">
-          <div className="h-full w-[350px] max-h-[320px] flex justify-center items-center border overflow-hidden border-primaryColor mr-4 mb-4 rounded-md">
-            <Image
-              src="/assets/images/5thstreetalley.jpg"
-              width={300}
-              height={400}
-              alt="daniel"
-              className="object-fill"
-            />
-          </div>
+        <section className="flex items-center w-full max-w-[1128px]  mb-4">
           <Quote />
         </section>
-        <section className="w-full  flex justify-center mb-4">
+        {/* <section className="w-full  flex justify-center mb-4">
           <MyCalendar />
-        </section>
+        </section> */}
       </Container>
     </main>
   );
