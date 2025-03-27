4d37e8c2434b23851f47d73c93b3ee82a1a60ebc
Hiro Nakae <32496402+hnakae@users.noreply.github.com>
2025-03-26 23:00:31 -0700

    socials

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -12,11 +12,11 @@ export default function Contact() {
       <Container>
         <Header />
         <div className="w-full flex justify-center items-start z-10 mb-4 text-neutralColor ">
-          <div className=" max-w-[1128px]  px-14 py-10 justify-around flex items-center rounded-md  border border-primaryColor  shadow-md   backdrop-blur-md w-full ">
+          <div className="text-neutralColor max-w-[1128px]  px-14 py-10 justify-around flex items-center rounded-md  border border-primaryColor  shadow-md   backdrop-blur-md w-full ">
             <ul>
               <Link
                 href="https://discord.com/channels/1164649557687275703/1164649557687275706"
-                className="text-blue-500 underline flex mb-3"
+                className="hover:underline hover:text-primaryColor flex mb-3"
                 target="_blank"
               >
                 <Image
@@ -30,7 +30,7 @@ export default function Contact() {
               </Link>
               <Link
                 href="https://www.meetup.com/eugene-go-players/?eventOrigin=event_home_page"
-                className="text-blue-500 underline flex mb-3"
+                className="hover:text-primaryColor  hover:underline flex mb-3"
                 target="_blank"
               >
                 <Image
@@ -44,7 +44,7 @@ export default function Contact() {
               </Link>
               <Link
                 href="https://www.facebook.com/groups/EugeneGoClub"
-                className="text-blue-500 underline flex mb-3"
+                className="hover:text-primaryColor  hover:underline flex mb-3"
                 target="_blank"
               >
                 <Image
@@ -57,18 +57,38 @@ export default function Contact() {
                 Facebook
               </Link>
               <Link
-                href="https://online-go.com/group/6435"
-                className="text-blue-500 underline flex mb-3"
+                href="https://baduk.club/club/eugene"
+                className="hover:text-primaryColor  hover:underline flex mb-3"
                 target="_blank"
               >
-                OGS - Online Go Server
+                <Image
+                  src="/icons/badukclub.png"
+                  alt="facebok icon"
+                  width={25}
+                  height={25}
+                  className="pointer-events-none mr-2"
+                />
+                Baduk.club
+              </Link>
+              <Link
+                href="https://online-go.com/group/6435"
+                className="hover:text-primaryColor  hover:underline flex mb-3"
+                target="_blank"
+              >
+                <Image
+                  src="/icons/ogs.png"
+                  alt="facebok icon"
+                  width={25}
+                  height={25}
+                  className="pointer-events-none mr-2"
+                />
+                OGS
               </Link>
             </ul>
             <ul>
               <div>Daniel Pono Takamori</div>
-              <div className="mb-3">971-533-7727</div>
+              <div className="">971-533-7727</div>
               <div>contact@eugenego.club</div>
-              <div>eugenego.contact@gmail.com</div>
             </ul>
           </div>
         </div>
