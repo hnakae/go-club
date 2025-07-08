af44bd7c9a8db0d50d34ae1bf290cf528d75fede
Hiro Nakae <32496402+hnakae@users.noreply.github.com>
2025-07-07 21:05:53 -0700

    github login via tina cloud

--- tina/config.ts
+++ tina/config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
       publicFolder: "public",
     },
   },
-  authProvider: new LocalAuthProvider(),
+  
   
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
   schema: {
